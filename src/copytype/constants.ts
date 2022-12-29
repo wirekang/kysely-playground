5d173d3a0595d5bdd836fec1cbd94f3c2f56f3dc
wirekang <wirekang@gmail.com>
2022-12-29 11:22:42 +0900

    feat: add ColumnType

--- src/copytype/constants.ts
+++ src/copytype/constants.ts
@@ -1,13 +1,14 @@
 import KyselyInfo from "../../node_modules/kysely/package.json";
 
 export const KYSELY_GLOBAL_TYPE = `
-import {Kysely, Compilable, Generated as kyselyGenerated, sql as kyselySql} from 'kysely';
+import {Kysely, Compilable,ColumnType as kyselyColumnType, Generated as kyselyGenerated, sql as kyselySql} from 'kysely';
 declare global {
   interface DB{};
   declare const kysely: Kysely<DB>;
   declare const db: Kysely<DB>;
   declare let result: Compilable<any>;
   type Generated<T> = kyselyGenerated<T>;
+  type ColumnType<S,I = S,U = S> = kyselyColumnType<S,I,U>;
   declare const sql = kyselySql;
 }
 `;
