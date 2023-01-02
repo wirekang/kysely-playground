ce304a0e67f65e6a85cc6b1c6bf827ee1259fc2e
wirekang <wirekang@gmail.com>
2023-01-02 19:43:10 +0900

    build: reduce versions

--- src/gen/kysely-modules.ts
+++ src/gen/kysely-modules.ts
@@ -2,22 +2,10 @@ import * as kysely_0_23_3 from "kysely_0_23_3";
 import * as kysely_0_23_2 from "kysely_0_23_2";
 import * as kysely_0_23_1 from "kysely_0_23_1";
 import * as kysely_0_23_0 from "kysely_0_23_0";
-import * as kysely_0_22_0 from "kysely_0_22_0";
-import * as kysely_0_21_6 from "kysely_0_21_6";
-import * as kysely_0_21_5 from "kysely_0_21_5";
-import * as kysely_0_21_4 from "kysely_0_21_4";
-import * as kysely_0_21_3 from "kysely_0_21_3";
-import * as kysely_0_21_2 from "kysely_0_21_2";
 
 export const MODULE_MAP = {
   kysely_0_23_3: kysely_0_23_3,
   kysely_0_23_2: kysely_0_23_2,
   kysely_0_23_1: kysely_0_23_1,
   kysely_0_23_0: kysely_0_23_0,
-  kysely_0_22_0: kysely_0_22_0,
-  kysely_0_21_6: kysely_0_21_6,
-  kysely_0_21_5: kysely_0_21_5,
-  kysely_0_21_4: kysely_0_21_4,
-  kysely_0_21_3: kysely_0_21_3,
-  kysely_0_21_2: kysely_0_21_2,
 } as const;
