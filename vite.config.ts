2508720205af950224ba5867835c1ae1f0891c13
wirekang <wirekang@gmail.com>
2023-01-03 10:30:38 +0900

    build: add bundle size visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/kysely-playground",
+  plugins: [visualizer()],
 });
