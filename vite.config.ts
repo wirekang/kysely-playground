1d0c77763f21373a2ef678285e0854d2578aefc4
wirekang <wirekang@gmail.com>
2023-01-03 10:34:01 +0900

    build: disable sourcemap and cache

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import { visualizer } from "rollup-plugin-visualizer";
 export default defineConfig({
   base: "/kysely-playground",
   plugins: [visualizer()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      cache: false,
+    },
+  },
 });
