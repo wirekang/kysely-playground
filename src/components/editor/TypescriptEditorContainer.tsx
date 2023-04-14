2ba7813320b41c1016d5e48ca1cb3f612c417261
wirekang <wirekang@gmail.com>
2023-04-14 17:30:26 +0900

    feat: update ui

--- src/components/editor/TypescriptEditorContainer.tsx
+++ src/components/editor/TypescriptEditorContainer.tsx
@@ -7,13 +7,20 @@ export function TypescriptEditorContainer({ children }: any): JSX.Element {
   const vertical = useMediaQuery({ query: "(max-width: 1000px)" })
   const compact = useIsCompact()
   const showTypescriptSchema = useRecoilValue(showTypescriptSchemaState)
+  let flex = 2
+  if (vertical && !compact) {
+    flex = 1.3
+  }
+  if (!showTypescriptSchema) {
+    flex = 1
+  }
 
   return (
     <div
       style={{
         display: "flex",
         flexDirection: vertical ? "column" : "row",
-        flex: (vertical && !compact) || !showTypescriptSchema ? 1 : 2,
+        flex,
         width: "100%",
         height: "100%",
         alignItems: "stretch",
