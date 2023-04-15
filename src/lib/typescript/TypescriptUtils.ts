d327528d34064d83fa005928a1877ca7ddbcf95e
wirekang <wirekang@gmail.com>
2023-04-15 14:25:38 +0900

    feat: ignore keywords

--- src/lib/typescript/TypescriptUtils.ts
+++ src/lib/typescript/TypescriptUtils.ts
@@ -2,8 +2,8 @@ export class TypescriptUtils {
   public static async toJs(ts: string): Promise<string> {
     const { ModuleKind, transpile } = await import("typescript")
     return transpile(ts, { module: ModuleKind.ES2020 })
-      .replace(/^\s*export {};?$/m, "")
-      .replace(/^\s*import .+ from .+$/m, "")
-      .replace(/\s+await\s+/, " ")
+      .replace(/^\s*export {};?$/gm, "")
+      .replace(/^\s*import .+ from .+$/gm, "")
+      .replace(/\s+await\s+/g, " ")
   }
 }
