980267200bc2911774518dcb367b86839603ec31
wirekang <wirekang@gmail.com>
2023-10-26 17:31:07 +0900

    feat: update global type comments

--- src/lib/typescript/TypescriptConstants.ts
+++ src/lib/typescript/TypescriptConstants.ts
@@ -1,24 +1,31 @@
+const hr = ["", "---", ""]
+const fromPlayground = ["@from_playground", "The whole comment is from playground.", ...hr]
+const mockType = [...fromPlayground, "@mock_type", "The original type definition is too large for it's worth.", ...hr]
+
+function comment(lines: string[], indent = 4): string {
+    const space = " ".repeat(indent)
+    return `/**\n${lines.map(v => `${space}* ${v}`).join("\n")}${space}*/`
+}
+
+const kyselyComment = comment([...fromPlayground, "Kysely instance for playground.", "You can use `db` instead."])
+const dbComment = comment([...fromPlayground, "Alias for `kysely`"])
+const bufferComment = comment([...mockType, "Node built-in Buffer"])
+
 export class TypescriptConstants {
-  public static readonly PLAYGROUND_GLOBAL_TYPE_CONTENT = `
+    public static readonly PLAYGROUND_GLOBAL_TYPE_CONTENT = `
   import {Kysely} from 'kysely';
   
   declare global {
     interface DB {}
     
-    /**
-    *  \`THIS MESSAGE IS FROM PLAYGROUND.\`
-    *
-    * Kysely instance for playground.
-    * You can use \`db\` instead.
-    */
+    ${kyselyComment}
     const kysely: Kysely<DB>
     
-    /**
-    *  \`THIS MESSAGE IS FROM PLAYGROUND.\`
-    *
-    * Alias for \`kysely\`.
-    */
+    ${dbComment}
     const db: Kysely<DB>
+    
+    ${bufferComment}
+    interface Buffer extends Uint8Array {}
   }
   `
 }
