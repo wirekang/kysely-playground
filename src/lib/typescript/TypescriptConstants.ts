d1f5fd6db4b68509b1e56ca2413728e56ed8d1d3
wirekang <wirekang@gmail.com>
2023-11-13 11:14:28 +0900

    feat: update global type comments

--- src/lib/typescript/TypescriptConstants.ts
+++ src/lib/typescript/TypescriptConstants.ts
@@ -1,18 +1,23 @@
 const hr = ["", "---", ""]
-const fromPlayground = ["@from_playground", "The whole comment is from playground.", ...hr]
-const mockType = [...fromPlayground, "@mock_type", "The original type definition is too large for it's worth.", ...hr]
+const playground = ["@playground", "The whole comment is from kysely-playground.", ...hr]
+const typeOmitted = [
+  ...playground,
+  "@type_omitted",
+  "The original type definition is too large for its worth at playground.",
+  ...hr,
+]
 
 function comment(lines: string[], indent = 4): string {
-    const space = " ".repeat(indent)
-    return `/**\n${lines.map(v => `${space}* ${v}`).join("\n")}${space}*/`
+  const space = " ".repeat(indent)
+  return `/**\n${lines.map((v) => `${space}* ${v}`).join("\n")}${space}*/`
 }
 
-const kyselyComment = comment([...fromPlayground, "Kysely instance for playground.", "You can use `db` instead."])
-const dbComment = comment([...fromPlayground, "Alias for `kysely`"])
-const bufferComment = comment([...mockType, "Node built-in Buffer"])
+const kyselyComment = comment([...playground, "Kysely instance for playground.", "You can also use `db`."])
+const dbComment = comment([...playground, "Alias for `kysely`"])
+const bufferComment = comment([...typeOmitted, "Node built-in Buffer"])
 
 export class TypescriptConstants {
-    public static readonly PLAYGROUND_GLOBAL_TYPE_CONTENT = `
+  public static readonly PLAYGROUND_GLOBAL_TYPE_CONTENT = `
   import {Kysely} from 'kysely';
   
   declare global {
