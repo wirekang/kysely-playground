d4eda7c4608bcbd2e9e4aad884a72b99124fa33f
wirekang <wirekang@gmail.com>
2024-01-16 14:48:18 +0900

    setup firebase

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPmIeokxQ2UW4nNDNz8GpvxECU3VVbM8k",
+  authDomain: "kyse-link.firebaseapp.com",
+  projectId: "kyse-link",
+  storageBucket: "kyse-link.appspot.com",
+  messagingSenderId: "570112190474",
+  appId: "1:570112190474:web:0c7f0ffd69f9c106e297f3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
